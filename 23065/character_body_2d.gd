362e2b67ac3a444db7604a8f875b5245406b78a3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-13 13:53:48 +1200

    yfyuftyfy

--- 23065/character_body_2d.gd
+++ 23065/character_body_2d.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
+var direction = Input.get_axis("ui_left", "ui_right")
 var SPEED = 190
-const JUMP_VELOCITY = -900
-
+const JUMP_VELOCITY = -550
+var sprinting = 0
 
 
 func _physics_process(delta):
@@ -14,8 +14,16 @@ func _physics_process(delta):
 
 	# HAndle Sprint
 	if Input.is_action_pressed("Shift") == true:
-		SPEED = 300
-	else: SPEED = 190
+		SPEED = 400
+		sprinting = 1
+	else: 
+		SPEED = 190
+		sprinting = 0
+	#Handle dash
+	if sprinting == 1 and Input.is_action_just_pressed("Space"):
+		velocity.x = direction * SPEED * 1000
+		print("dasj")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
